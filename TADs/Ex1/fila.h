f9a8957a8799240c4b2ed4979a5ef49be6e13dcc
Otávio Augusto <tavinteixeira@hotmail.com>
2023-03-20 09:57:01 -0300

    comentarios ex1

--- TADs/Ex1/fila.h
+++ TADs/Ex1/fila.h
@@ -1,9 +1,13 @@
 typedef struct fila Tfila;
 
+//função que insere um valor n em uma fila Tfila fila no final da fila
 void insere(Tfila **fila,int n);
 
+//função que remove o primeiro da fila e devolve o valor do mesmo
 int remover(Tfila **fila);
 
+//função que printa a fila em ordem
 void returnFila(Tfila *fila);
 
+//função que libera a memoria da fila
 void liberarFila(Tfila **fila);
